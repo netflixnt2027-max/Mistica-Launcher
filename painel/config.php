fc2fae76bede781cac56a8ee993a361cfb5c66ea
netflixnt2027-max <netflixnt2027@gmail.com>
2026-09-18 19:12:38 -0300

    Update config.php

--- painel/config.php
+++ painel/config.php
@@ -1,4 +1,4 @@
 <?php
 // Troque antes de enviar ao InfiniteFree. Use uma senha forte e exclusiva.
-define('SENHA_ADMIN', 'troque-esta-senha');
+define('SENHA_ADMIN', 'semsenha120238');
 define('ARQUIVO_DADOS', __DIR__ . '/dados.json');
